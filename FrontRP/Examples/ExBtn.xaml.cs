ce98d66a174130dc525bbc7de73be7e871cccbdf
amr-swalha <amr.swalha.code@outlook.com>
2017-01-22 19:01:35 +0200

    Adding button example

---
+++ FrontRP/Examples/ExBtn.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FrontRP.Examples
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ExBtn : Page
+    {
+        public ExBtn()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BtnMain_OnClick(object sender, RoutedEventArgs e)
+        {
+            /*
+             When the button is pressed
+            */
+        }
+
+        private void BtnMain_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            /*When the button is on foucs*/
+        }
+
+        private void BtnMain_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            /*When the button is of foucs*/
+        }
+    }
+}
