71ea0b53f50b552e11c39a7a359cf848b21f9786
amr-swalha <amr.swalha.code@outlook.com>
2017-01-25 22:56:29 +0200

    Adding StackPanel & ScrollViewer Example

--- FrontRP/Examples/ExScrollView.xaml.cs
+++ FrontRP/Examples/ExScrollView.xaml.cs
@@ -12,25 +12,21 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using FrontRP.Examples;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace FrontRP
+namespace FrontRP.Examples
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class ExScrollView : Page
     {
-        public MainPage()
+        public ExScrollView()
         {
             this.InitializeComponent();
         }
 
-        private void ClickMe_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(ExScrollView), null);
-        }
+       
     }
 }
